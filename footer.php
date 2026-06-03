da47db4302f8bb92e7cc6adde75f2b2dac287f99
Paula0M <paulamirazf@gmail.com>
2026-06-03 13:40:53 +0200

    first_code

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p>© <?php echo date("Y"); ?> Happy Meal</p>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="header">
+        div class="container">
+            <h1 class="logo"><?php bloginfo('name'); ?></h1>
+            <nav class="nav">
+               <a href="#">Inicio</a>
+               <a href="#">Productos</a>
+               <a href="#">Contacto</a>
+               </a>
+            </nav>
+            <div class=""cart-icon>🛒</div>
+        </div>
+    </header>
